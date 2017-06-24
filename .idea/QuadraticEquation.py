32116bc8dd8364c8b49fc1e7089fb2e0a243faa6
QueraltSosaMompel <queraltsosa@hotmail.com>
2017-06-24 20:10:34 +0100

    Initial commit

---
+++ .idea/QuadraticEquation.py
@@ -0,0 +1,40 @@
+from math import sqrt
+
+def QuadraticEquation(a, b, c):
+    if (b*b<4*a*c):
+        return ("\nThe solution belongs to the Complex Numbers field.");
+
+    elif ((a!=0) and ((b*b)>(4*a*c))) :
+        solution = "\nFirst Solution: " + str((-b+sqrt(b*b-4*a*c))/(2*a)) + \
+                   "\nSecond Solution: "+ str((-b-sqrt(b*b-4*a*c))/(2*a));
+        return solution
+
+    else:
+        return ("\nThis equation is not a second degree one.");
+
+
+if __name__ == "__main__":
+    print("Test 1: " + str(QuadraticEquation(2,2,2)) + "\n")
+    print("Test 2: " + str(QuadraticEquation(1,-5,6)) + "\n")
+    print("Test 3: " + str(QuadraticEquation(0,0,0)) + "\n")
+
+
+
+    """
+    Console results:
+
+    /usr/bin/python /Users/QSM/QSM/QuadraticEquation/.idea/QuadraticEquation.py
+    Test 1:
+    The solution belongs to the Complex Numbers field.
+
+    Test 2:
+    First Solution: 3.0
+    Second Solution: 2.0
+
+    Test 3:
+    This equation is not a second degree one.
+
+
+    Process finished with exit code 0
+
+    """
